0fb726339c488b9756fdf424a9bd9eac53333fe7
Echoidf <Echoidf@users.noreply.github.com>
2023-07-16 11:01:01 +0000

    deploy: 0575fffd1ceb8e68eaa04898828461fd89f635c6

---
+++ static/js/index-8d191c53.js
@@ -0,0 +1,1 @@
+import{d as e,g as n,b as o}from"./index-aec91119.js";const c=e({name:"Welcome",__name:"index",setup(a){return(t,m)=>(o(),n("h1",null,"Fireboom-admin"))}});export{c as default};
--- static/js/index-b854d5e8.js
+++
@@ -1,1 +0,0 @@
-import{d as e,g as n,b as o}from"./index-d09e3f55.js";const c=e({name:"Welcome",__name:"index",setup(a){return(t,m)=>(o(),n("h1",null,"Fireboom-admin"))}});export{c as default};
